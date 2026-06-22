b0fefd671b17d160458f7cf3a6b0c234dc2d866e
tadasatgithub <reachtanmay1982@gmail.com>
2026-06-22 10:01:36 +0000

    EPI 19.2: Paint a boolean matrix.

--- epi_judge_java/epi/MatrixConnectedRegions.java
+++ epi_judge_java/epi/MatrixConnectedRegions.java
@@ -4,11 +4,40 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+
 public class MatrixConnectedRegions {
   public static void flipColor(int x, int y, List<List<Boolean>> image) {
-    // TODO - you fill in here.
-    return;
+    int maxx=image.size();
+    int maxy=image.get(0).size();
+    Queue<int[]> queue = new LinkedList<>();
+    int[][] track = new int[maxx][maxy];
+
+    queue.offer(new int[] {x, y});
+    int[][] directions = new int[][] {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    boolean color = image.get(x).get(y);
+    track[x][y] = 1; // waiting
+    image.get(x).set(y, !color);
+
+    while (!queue.isEmpty()) {
+      int[] cur = queue.poll();
+
+      for (int[] dir : directions) {
+        int nx = cur[0] + dir[0];
+        int ny = cur[1] + dir[1];
+
+        if (nx < 0 || nx >= maxx || ny < 0 || ny >= maxy || track[nx][ny] != 0 || image.get(nx).get(ny) != color) {
+          continue;
+        }
+
+        track[nx][ny] = 1;
+        image.get(nx).set(ny, !color);
+        queue.offer(new int[] {nx, ny});
+      }
+    }
+    
   }
   @EpiTest(testDataFile = "painting.tsv")
   public static List<List<Integer>> flipColorWrapper(TimedExecutor executor,
